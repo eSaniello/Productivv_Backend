ec921f7af926cb43b89186829c70f25117fff0b3
eSaniello <shaniel.samadhan.natin@gmail.com>
2019-01-22 19:53:45 -0300

    working on taskController.js

--- server/controllers/taskControllers.js
+++ server/controllers/taskControllers.js
@@ -2,6 +2,8 @@ const sequelize = require('sequelize');
 const dbConfig = require('../config/config');
 const connection = new sequelize(dbConfig[process.env.ENVIRONMENT || 'development']);
 
+const gebruikers_model = require('./userControllers');
+
 const taken = connection.define('taken', {
     taak_id: {
         type: sequelize.INTEGER,
@@ -18,58 +20,14 @@ const taken = connection.define('taken', {
     categorie: sequelize.STRING
 })
 
+taken.belongsTo(gebruikers_model, {
+    foreignKey: 'gebruikers_id',
+    targetKey: 'gebruikers_id'
+});
+
 //CREATE
-function Create(req, res){
-    taken.create({
-        gebruikers_naam: req.body.gebruikers_naam,
-        voornaam: req.body.voornaam,
-        achternaam: req.body.achternaam,
-        email: req.body.email,
-        wachtwoord: req.body.wachtwoord
-    }).then(gebruikers => res.json(gebruikers)).catch(error => {
-        res.json({ message: error })
-    });
-}
-
-//READ
-function SelectAll(req, res){
-    taken.findAll().then(gebruikers => {
-        res.json(gebruikers);
-    }).catch(error => {
-        res.json({message: error});
-    });
-}
-
-function SelectOne(req, res) {
-    taken.findOne({
-        where: { gebruikers_id: req.params.gebruikers_id } 
-    }).then(gebruikers => res.json(gebruikers)).catch(error => {
-        res.json({message: error})
-    });
-}
-
-//UPDATE
-function Update(req, res){
-    taken.update({
-        gebruikers_naam: req.body.gebruikers_naam,
-        voornaam: req.body.voornaam,
-        achternaam: req.body.achternaam,
-        email: req.body.email,
-        wachtwoord: req.body.wachtwoord
-    }, {
-        where: {gebruikers_id : req.body.gebruikers_id}
-    }).then(gebruikers => res.json(gebruikers)).catch(error => {
-        res.json({message: error})
-    });
-}
-
-//DELETE
-function DeleteOne(req, res){
-    taken.destroy({
-        where: {gebruikers_id: req.body.gebruikers_id}
-    }).then(gebruikers => res.json(gebruikers)).catch(error => {
-        res.json({message: error})
-    });
+function Create(){
+    taken.create
 }
 
 module.exports = {
