6fc4277a3600a6aec9f6af6e920f1636722be172
esaniello <shaniel.samadhan.natin@gmail.com>
2019-02-16 17:45:54 -0300

    testing heroku

--- server/config/config.js
+++ server/config/config.js
@@ -1,19 +1,19 @@
 const db = {
     development: {
-        host: 'localhost',
-        username: 'postgres',
-        password: "dzdvml114",
-        database: 'productivv',
+        host: 'ec2-54-221-253-228.compute-1.amazonaws.com',
+        username: 'ffwluocyeqcvpf',
+        password: "3dec9fbd6dc551c401027318ec5680f0f3fd4aefae7a3b027ea6624da31b8868",
+        database: 'd3gnr1pmfu0f2g',
         dialect: 'postgres',
         define: {
             timestamps: false
         }
     },
     production: {
-        host: 'localhost',
-        username: 'root',
-        password: "",
-        database: 'productivv',
+        host: 'ec2-54-221-253-228.compute-1.amazonaws.com',
+        username: 'ffwluocyeqcvpf',
+        password: "3dec9fbd6dc551c401027318ec5680f0f3fd4aefae7a3b027ea6624da31b8868",
+        database: 'd3gnr1pmfu0f2g',
         dialect: 'postgres',
         define: {
             timestamps: false
